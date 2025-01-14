99aa9e671a1d1eb29ca72c807fc95747448ebb42
felix-atoma <felixatoma2@gmail.com>
2025-01-14 09:56:27 +0000

    added navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import "./navbar.css";
+import "./Navbar.css";
 import data from "./Data";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
