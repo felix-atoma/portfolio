127031877df41945bcd12fa117e0c52c23e74863
felix-atoma <felixatoma2@gmail.com>
2025-01-14 15:08:40 +0000

    added functionality for the form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,59 +1,58 @@
-import { useForm, ValidationError } from '@formspree/react'
-
-import './Contact.css'
+import { useForm, ValidationError } from '@formspree/react';
+import './Contact.css';
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm("xblrwjey");
+  // Use the new Formspree endpoint
+  const [state, handleSubmit] = useForm("mannkwnr");
+  
   if (state.succeeded) {
-    return <div className='success'>
-      <h3>Thank you.I appreciate your message. Will respond ASAP.</h3>
-    </div>;
+    return (
+      <div className="success">
+        <h3>Thank you. I appreciate your message. Will respond ASAP.</h3>
+      </div>
+    );
   }
+
   return (
     <section id="contact">
-      <h5> Contact</h5>
+      <h5>Contact</h5>
       <h2>Let's <span>Connect</span></h2>
 
       <div className="form_container">
-        <form onSubmit={handleSubmit} action='https://formspree.io/f/xblrwjey'>
-          <label htmlFor="email">
-            Email Address
-          </label>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email Address</label>
           <input
             id="email"
             type="email"
             name="email"
+            required
           />
-          <label htmlFor="text">
-            Company Name
-          </label>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+          <label htmlFor="company">Company Name</label>
           <input
             id="company"
             type="text"
             name="name"
+            required
           />
-          <ValidationError
-            prefix="Email"
-            field="email"
-            errors={state.errors}
-          />
+
+          <label htmlFor="message">Message</label>
           <textarea
             id="message"
             name="message"
-            placeholder='Enter your message....'
+            placeholder="Enter your message..."
+            required
           />
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
-          <button type="submit" disabled={state.submitting} className='btn'>
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+          <button type="submit" disabled={state.submitting} className="btn">
             Submit
           </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
