c7044fffc2225b5457a78710dc392c1e5da3909d
felix-atoma <felixatoma2@gmail.com>
2025-01-17 15:10:12 +0000

    added new features

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,58 +1,59 @@
-import { useForm, ValidationError } from '@formspree/react';
-import './Contact.css';
+import { useForm, ValidationError } from '@formspree/react'
+
+import './contact.css'
 
 const Contact = () => {
-  // Use the new Formspree endpoint
-  const [state, handleSubmit] = useForm("mannkwnr");
-  
+  const [state, handleSubmit] = useForm("xblrwjey");
   if (state.succeeded) {
-    return (
-      <div className="success">
-        <h3>Thank you. I appreciate your message. Will respond ASAP.</h3>
-      </div>
-    );
+    return <div className='success'>
+      <h3>Thank you.I appreciate your message. Will respond ASAP.</h3>
+    </div>;
   }
-
   return (
     <section id="contact">
-      <h5>Contact</h5>
+      <h5> Contact</h5>
       <h2>Let's <span>Connect</span></h2>
 
       <div className="form_container">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email Address</label>
+        <form onSubmit={handleSubmit} action='https://formspree.io/f/mannkwnr'>
+          <label htmlFor="email">
+            Email Address
+          </label>
           <input
             id="email"
             type="email"
             name="email"
-            required
           />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-
-          <label htmlFor="company">Company Name</label>
+          <label htmlFor="text">
+            Company Name
+          </label>
           <input
             id="company"
             type="text"
             name="name"
-            required
           />
-
-          <label htmlFor="message">Message</label>
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
           <textarea
             id="message"
             name="message"
-            placeholder="Enter your message..."
-            required
+            placeholder='Enter your message....'
           />
-          <ValidationError prefix="Message" field="message" errors={state.errors} />
-
-          <button type="submit" disabled={state.submitting} className="btn">
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button type="submit" disabled={state.submitting} className='btn'>
             Submit
           </button>
         </form>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import './Portfolio.css'
+
+import immo from '../assets/immo.png'
+import farm from '../assets/farm.png'
+import grocery from '../assets/grocery.png'
+import dopel from '../assets/dopel.jpg'
+import nashma from '../assets/nashma.png'
+import espc from '../assets/espc.png'
+import { FaLink } from "react-icons/fa6";
+
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>Portfolio</h5>
+      <h2>Latest <span>Projects</span></h2>
+
+      <div className="portfolio-content">
+        <div className="row">
+          <img src={dopel} alt="" />
+          <div className="layer">
+            <h2>
+              A web app to create a  portfolio. A collaborated project by Team DOPEL still in progress
+            </h2>
+            <div className="link">
+              <a href="https://dopel.netlify.app/"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img src={immo} alt="" />
+          <div className="layer">
+            <h2>
+            Frontend team member on ImmoAsk, simplifying accommodation searches in Togo
+            </h2>
+            <div className="link">
+              <a href="https://www.immoask.com/tg"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img src={farm} alt="" />
+          <div className="layer">
+            <h2>
+            Frontend partner for FarmConnect Ghana, bridging farmers and customers for fresh, direct farm produce 
+            </h2>
+            <div className="link">
+              <a href="https://farmconnectgh.netlify.app/"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img src={grocery} alt="" />
+          <div className="layer">
+            <h2>
+            Redefining grocery shopping—an app that makes buying essentials seamless and effortless
+            </h2>
+            <div className="link">
+              <a href="https://nashama.netlify.app/"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img src={nashma} alt="" />
+          <div className="layer">
+            <h2>
+            Crafted Nashma Agribusiness website—empowering growth and innovation in agriculture with a dynamic online presence
+            </h2>
+            <div className="link">
+              <a href="https://nashma-agribusness.netlify.app/"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img src={espc} alt="" />
+          <div className="layer">
+            <h2>
+            Built Ecole St. Pierre Claver's website with online enrollment and teaching features.
+            </h2>
+            <div className="link">
+              <a href="https://ecolestpierre.org/"><FaLink /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Portfolio
+export default Portfolio;
